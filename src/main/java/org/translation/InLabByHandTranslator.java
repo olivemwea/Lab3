17f95760cae8aefbd9ee430717752d99e0d8c978
Olive Wachira <o.wachira@mail.utoronto.ca>
2024-09-27 14:04:34 -0400

    done todos except checkstlye can

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -3,10 +3,6 @@ package org.translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task: modify this class so that it also supports the Spanish language code "es" and
-//            one more language code of your choice. Each member of your group should add
-//            support for one additional langauge code on a branch; then push and create a pull request on GitHub.
-
 // Extra Task: if your group has extra time, you can add support for another country code in this class.
 
 /**
@@ -14,6 +10,9 @@ import java.util.List;
  * the country code "can" to several languages.
  */
 public class InLabByHandTranslator implements Translator {
+
+    public static final String CANADA = "can";
+
     /**
      * Returns the language abbreviations for all languages whose translations are
      * available for the given country.
@@ -21,17 +20,15 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+
     @Override
     public List<String> getCountryLanguages(String country) {
         // TODO Checkstyle: The String "can" appears 4 times in the file.
         if ("can".equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
+            return new ArrayList<>(List.of("de", "en", "zh", "es", "fa", "it"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -53,22 +50,28 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
+        if (!"can".equals(country)) {
             return null;
         }
-        if (language.equals("de")) {
-            return "Kanada";
+        String result = null;
+        if ("de".equals(language)) {
+            result = "Kanada";
         }
-        else if (language.equals("en")) {
-            return "Canada";
+        else if ("en".equals(language)) {
+            result = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            result = "加拿大";
         }
-        else {
-            return null;
+        else if ("es".equals(language)) {
+            result = "Canadá";
         }
+        else if ("fa".equals(language)) {
+            result = "کانادا";
+        }
+        else if ("it".equals(language)) {
+            result = "Canada";
+        }
+        return result;
     }
 }
